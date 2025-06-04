8d2497d69272f265e2984e86ccd824abaf0fb8c7
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-06-04 15:25:38 +0300

    userdata

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+// import './App.css'
 
-import './App.css'
+// function App() {
 
-function App() {
+//   return (
+//     <>
+   
+//     </>
+//   )
+// }
 
+// export default App
+
+import userData from "./userData.json";
+import Profile from './components/Profile';
+
+export default function App() {
   return (
     <>
-   
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
     </>
-  )
-}
-
-export default App
+  );
+};
